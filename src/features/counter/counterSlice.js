4a1faa0f6c1eb2d296effdece68d522bfb0c645c
aloshvp <aloshvp12@gmail.com>
2025-09-24 00:00:26 +0530

    counter updates

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -23,6 +23,6 @@ export const counterSlice = createSlice({
     }
 });
 
-export const { increment, decrement, reset, } = counterSlice.actions;
+export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions;
 
 export default counterSlice.reducer;
