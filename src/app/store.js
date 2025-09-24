b83066f4c64ef692d1db64987d97f67ad420ad24
aloshvp <aloshvp12@gmail.com>
2025-09-24 23:07:42 +0530

    user store & slice added

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice"
 import postReducer from "../features/post/postSlice"
+import userReducer from "../features/users/usersSlice"
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
-        posts: postReducer
+        posts: postReducer,
+        users: userReducer
     }
 })
